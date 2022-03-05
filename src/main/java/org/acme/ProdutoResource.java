5a9a7e0e0b8120a35ecd8a5d02c912532d1eac06
Jean Barcellos <jeanbarcellos@hotmail.com>
2022-03-05 17:17:57 -0300

    Criação do Resource

---
+++ src/main/java/org/acme/ProdutoResource.java
@@ -0,0 +1,21 @@
+package org.acme;
+
+import java.util.List;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/produtos")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+public class ProdutoResource {
+
+    @GET
+    public List<Produto> buscarTodosProdutos() {
+        return Produto.listAll();
+    }
+
+}
